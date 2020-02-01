0eadf810d449312711ce49f507c5dd0e22688877
Catherine Smiley <catherinesmiley@gmail.com>
2020-02-01 22:35:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -80,8 +80,7 @@ class Song
   end 
   
   def artist_name=(name)
-    Artist.find_or_create_by_name(name)
-    self.artist = name 
+    self.artist = Artist.find_or_create_by_name(name)
   end 
   
 end 
