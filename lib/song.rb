bb18d20387c18b499faeb84b7afa025ef278ebd7
Catherine Smiley <catherinesmiley@gmail.com>
2020-02-01 22:32:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -74,7 +74,7 @@ class Song
   def self.new_by_filename(filename)
     song = filename.split(" - ")[1] 
     artist = filename.split(" - ")[0]
-    new_song = self.new(filename)
+    new_song = self.new()
     new_song.name = song 
     new_song.artist = artist  
     new_song 
