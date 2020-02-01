a661cf5ce01911c4a05c71ff606d553f16894666
Catherine Smiley <catherinesmiley@gmail.com>
2020-02-01 22:13:56 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,42 +1,42 @@
-class Artist 
+# class Artist 
   
-  attr_accessor :name
+#   attr_accessor :name
   
-  @@all = []
+#   @@all = []
   
-  def initialize(name)
-    @name = name 
-    save
-  end
+#   def initialize(name)
+#     @name = name 
+#     save
+#   end
   
-  def self.all 
-    @@all 
-  end
+#   def self.all 
+#     @@all 
+#   end
   
-  def songs 
-    Song.all.select {|song| song.artist == self}
-  end 
+#   def songs 
+#     Song.all.select {|song| song.artist == self}
+#   end 
   
-  def add_song(song)
-    song.artist = self 
-  end 
+#   def add_song(song)
+#     song.artist = self 
+#   end 
   
-  def save 
-    @@all << self 
-  end 
+#   def save 
+#     @@all << self 
+#   end 
   
-  def self.find_or_create_by_name(name)
-    existing_artist = self.all.find {|artist| artist.name == name}
-    if existing_artist
-      existing_artist
-    else 
-      new_artist = self.new(name)
-      new_artist
-    end
-  end 
+#   def self.find_or_create_by_name(name)
+#     existing_artist = self.all.find {|artist| artist.name == name}
+#     if existing_artist
+#       existing_artist
+#     else 
+#       new_artist = self.new(name)
+#       new_artist
+#     end
+#   end 
   
-  def print_songs 
-    songs.each {|song| puts song.name}
-  end 
+#   def print_songs 
+#     songs.each {|song| puts song.name}
+#   end 
   
-end
+# end
