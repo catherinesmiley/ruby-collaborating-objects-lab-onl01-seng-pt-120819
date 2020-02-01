268da82b89384a4c415d6f699499e1a06cfe9deb
Catherine Smiley <catherinesmiley@gmail.com>
2020-02-01 22:14:14 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,17 +1,17 @@
-class MP3Importer 
+# class MP3Importer 
   
-  attr_accessor :path 
+#   attr_accessor :path 
   
-  def initialize(path)
-    @path = path 
-  end 
+#   def initialize(path)
+#     @path = path 
+#   end 
   
-  def files 
-    Dir.entries(@path).grep(/.mp3/)
-  end 
+#   def files 
+#     Dir.entries(@path).grep(/.mp3/)
+#   end 
   
-  def import
-    files.each {|filename| Song.new_by_filename(filename)}
-  end 
+#   def import
+#     files.each {|filename| Song.new_by_filename(filename)}
+#   end 
   
-end
+# end
